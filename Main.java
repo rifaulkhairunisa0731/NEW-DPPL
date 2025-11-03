d6becb154f90cae15e959fe89eecfbb73e5b6ab0
rifaulkhairunisa0731 <rifaul.khairunisa0731@student.unri.ac.id>
2025-11-03 10:05:25 +0700

    menambahkan nim

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nama  : Paul");
-        System.out.println("NIM   : 1234 ");
+        System.out.println("Nama  : PAUL");
+        System.out.println("NIM   : 2407110731 ");
 
         System.out.println("Nama  : Maira");
         System.out.println("NIM   : 2345");
