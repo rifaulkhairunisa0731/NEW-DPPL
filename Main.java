9e744ce3d368efe73d1746c2f8c06aa662ca9a98
rifaulkhairunisa0731 <rifaul.khairunisa0731@student.unri.ac.id>
2025-11-09 10:38:57 +0700

    menambahkan StudentRecord

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Nama  : PAUL");
-        System.out.println("NIM   : 2407110731 ");
-
-        System.out.println("Nama  : Maira");
-        System.out.println("NIM   : 2345");
-    }
-}
